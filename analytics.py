782ba116453fe6b847146db26282a7298ab2d59b
Filippo Leone <filippo.leone@airhelp.com>
2019-10-21 16:18:38 +0200

    Added analytics class info

---
+++ analytics.py
@@ -0,0 +1,12 @@
+import requests
+
+class GAHit:
+    """
+    Todo: @Filippo write GA class that will send hits 
+    to a new property so that we can track app usage / errors. 
+    """
+    def __init__(self):
+        pass
+
+    def send_hit(self, event):
+        pass
