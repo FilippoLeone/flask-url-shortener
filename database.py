b80d260b2634c141cb4f0ed8cd0f0f7d37471fbd
Filippo Leone <filippo.leone@airhelp.com>
2019-10-22 12:14:17 +0200

    Made some table fields unique

--- database.py
+++ database.py
@@ -25,7 +25,7 @@ class connector:
             cursor = connection.cursor()
             cursor.execute("""
             CREATE TABLE url_list
-            (id INTEGER PRIMARY KEY AUTOINCREMENT, full_url TEXT, shortlink TEXT, creation_date TEXT, expiration_date TEXT)
+            (id INTEGER PRIMARY KEY AUTOINCREMENT, full_url TEXT, shortlink TEXT UNIQUE, creation_date TEXT, expiration_date TEXT)
             """)
             connection.commit()
         except (sqlite3.DatabaseError, sqlite3.ProgrammingError, sqlite3.OperationalError) as err:
@@ -40,7 +40,7 @@ class connector:
             # Creating API key table
             cursor.execute("""
             CREATE TABLE api_keys
-            (id INTEGER PRIMARY KEY AUTOINCREMENT, owner TEXT, key TEXT, permissions TEXT)
+            (id INTEGER PRIMARY KEY AUTOINCREMENT, owner TEXT UNIQUE, key TEXT UNIQUE, permissions TEXT)
             """)
             connection.commit()
         except (sqlite3.DatabaseError, sqlite3.ProgrammingError, sqlite3.OperationalError) as err:
